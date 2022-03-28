02e58809bf8e742d4f76374a90135d592b42b6fa
isefrog38 <isefroge38@gmail.com>
2022-03-28 23:57:41 +0300

    other

--- src/components/SmallComponents/ThemeSelect/Theme.tsx
+++ src/components/SmallComponents/ThemeSelect/Theme.tsx
@@ -5,14 +5,15 @@ import moon from "../../../Assets/mini img/moon.png";
 
 export const Theme = () => {
 
-    const [showTheme, setShowTheme] = useState(false);
+    const [showTheme, setShowTheme] = useState<boolean>(false);
+
 
     const toggle = document.getElementById('toggle');
     const body = document.body;
 
     if (toggle) {
         toggle.addEventListener('input', (e) => {
-            if (!showTheme) {
+            if (showTheme) {
                 body.classList.add('dark-theme');
             } else {
                 body.classList.remove('dark-theme');
@@ -20,14 +21,22 @@ export const Theme = () => {
         });
     }
 
+
+    const onClickHandler = () => {
+        if (showTheme) {
+            return setShowTheme(false)
+        }
+        return setShowTheme(true)
+    }
+
     return (
         <div>
             <div className={s.toggle}>
                 <input type="checkbox" id="toggle"/>
-                <label htmlFor="toggle" onClick={() => setShowTheme(!showTheme)}>
-                    {!showTheme
+                <label htmlFor="toggle" onClick={onClickHandler}>
+                    {showTheme
                         ? <div className={s.main_image_sun}><img src={sun} alt={"sun"} className={s.image}/></div>
-                        : <div className={s.main_image_moon}><img src={moon} alt={"moon"} className={s.image} /></div>
+                        : <div className={s.main_image_moon}><img src={moon} alt={"moon"} className={s.image}/></div>
                     }
                 </label>
             </div>
